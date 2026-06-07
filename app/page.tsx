591523b54cf0380bf71b95b5672885f46c5b5939
Xaaf <x.vandalen@outlook.com>
2026-06-07 17:14:10 +0200

    chore: removed double Container nests; in traits also replaced fetch

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,32 @@
-import Container from "@/app/components/layout/Container";
 import NewsCard from "@/app/components/news/NewsCard";
 
 export default async function Home() {
     return (
-        <div className="">
-            <Container>
-                {/* Quick About / Intro */}
-                <section className="text-5xl text-center -mt-10 mb-2">
-                    <h1><strong>LATEST NEWS</strong></h1>
-                </section>
+        <>
+            <section className="text-5xl text-center -mt-10 mb-2">
+                <h1><strong>LATEST NEWS</strong></h1>
+            </section>
 
-                <main className="space-y-6 shadow-none">
-                    <NewsCard
-                        title="Coromon Remastered update is out on Steam!"
-                        excerpt="An update with a major UI overhaul has arrived on Steam. The update also includes
-                                    a number of bugfixes and some engine upgrades! In this post, we'll go over all
-                                    the changes made in the update."
-                        date="March 19th, 2026"
-                        link="news/patch-coromon-remastered"
-                        image="/images/coromon_remastered.png"
-                    />
+            <main className="space-y-6 shadow-none">
+                <NewsCard
+                    title="Coromon Remastered update is out on Steam!"
+                    excerpt="An update with a major UI overhaul has arrived on Steam. The update also includes
+                                a number of bugfixes and some engine upgrades! In this post, we'll go over all
+                                the changes made in the update."
+                    date="March 19th, 2026"
+                    link="news/patch-coromon-remastered"
+                    image="/images/coromon_remastered.png"
+                />
 
-                    <NewsCard
-                        title="Coromon Rogue Planet - September Progress"
-                        excerpt="The developers recently shared information on their progress with the spin-off
-                                    Coromon Rogue Planet. In this post, we'll discuss the newly shared information!"
-                        date="September 16th, 2025"
-                        link="news/rogue-planet-september-update"
-                        image="/images/coromon_rogue_planet.png"
-                    />                        
-                </main>
-            </Container>
-        </div>
+                <NewsCard
+                    title="Coromon Rogue Planet - September Progress"
+                    excerpt="The developers recently shared information on their progress with the spin-off
+                                Coromon Rogue Planet. In this post, we'll discuss the newly shared information!"
+                    date="September 16th, 2025"
+                    link="news/rogue-planet-september-update"
+                    image="/images/coromon_rogue_planet.png"
+                />                        
+            </main>
+        </>
     );
 }
--- app/traits/[slug]/page.tsx
+++ app/traits/[slug]/page.tsx
@@ -1,30 +1,20 @@
-import Container from "@/app/components/layout/Container";
 import TraitDetail from "@/app/components/traits/TraitDetail";
+import { getTraits } from "@/lib/utils/traitUtils";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
     const { slug } = await params;
 
-    const res = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL}/api/traits?search=${encodeURIComponent(slug)}`,
-        { next: { revalidate: 60 * 60 * 24 } }
-    );
-    const data = await res.json();
-
-    const trait = data.find((t: any) => t.slug === slug);
+    const traitList = await getTraits(slug);
+    const trait = traitList?.find((t: any) => t.slug === slug);
 
     if (!trait) {
-        return (
-            <div className="min-h-screen flex items-center justify-center text-xl text-center text-[var(--color-text)] dark:text-[var(--color-text-dark)] bg-[var(--color-bg)] dark:bg-[var(--color-bg-dark)]">
-                Trait not found. Please check the URL! If you believe this is a mistake, please contact the site admin.
-            </div>
-        );
+        notFound();
     }
 
     return (
         <div className="min-h-screen bg-[var(--color-bg)] dark:bg-[var(--color-bg-dark)] text-[var(--color-text)] dark:text-[var(--color-text-dark)]">
-            <Container>
-                <TraitDetail trait={trait} />
-            </Container>
+            <TraitDetail trait={trait} />
         </div>
     );
 }
